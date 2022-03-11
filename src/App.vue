47d1a613521fbfb26182e5ec75b8423d7a08a2a1
Don Forrest <forrestusbal@gmail.com>
2022-03-12 00:20:02 +0800

    Initialize AddTodo component functions and styles

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <div class="container"></div>
     <h1>Todo List</h1>
     <h6>Powered by: Ruby on Rails 6 | Vue 3 | Vuex 4 | PostgreSQL</h6>
+    <AddTodo />
     <TodoList />
   </div>
 </template>
 
 <script>
 import TodoList from "./components/TodoList.vue";
+import AddTodo from "./components/AddTodo.vue";
 
 export default {
   name: "App",
   components: {
     TodoList,
+    AddTodo
   },
 };
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h3></h3>
+    <div class="addTodo">
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex"
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    }
+  },
+  methods: {
+    ...mapActions(["addTodo"]),
+    onSubmit(event) {
+      event.preventDefault()
+      this.addTodo(this.title)
+      this.title = ""
+    }
+  }
+}
+</script>
+
+<style>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 0.625rem;
+  border: 1px solid #ccc;
+  outline: 0;
+}
+
+input[type="text"]:hover, input[type="text"]:focus {
+  border: 1px solid #41b883;
+}
+
+input[type="submit"] {
+  flex: 2;
+  color: #fff;
+  background: #41b883;
+  border: 1px solid #41b883;
+  cursor: pointer;
+  transition: 150ms ease-in-out;
+}
+
+input[type="submit"]:hover, input[type="submit"]:focus {
+  background: #318b63;
+}
+</style>
